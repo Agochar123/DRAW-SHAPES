fbcd0198ca9871797e30dc50ae40a9ebc7a4b35f
Agochar123 <94464716+Agochar123@users.noreply.github.com>
2021-12-29 19:17:12 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -4,17 +4,27 @@ import random
 dict1={"Shape1":"square","Shape2":"triangle","Shape3":"circle"}
 colors=["orange","red","green","blue","yellow"]
 
-#Take user input for the shape and use variable name as user_input
+user_input=input("enter the shape that you want to draw")
 
-#Fill colors using random function
+turtle.fillcolor(random.choice(colors))
 turtle.begin_fill()
 
 if dict1.get("Shape1")==user_input:
-    #Write the code for drawing a square having side = 100
-elif #use the get function using key==user_input:
-    #Write the code for drawing a triangle having side = 100
-elif #use the get function using key==user_input:
-    #Draw a square of radius = 100
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+elif dict1.get("Shape2")==user_input:
+    turtle.forward(100)
+    turtle.left(120)
+    turtle.forward(100)
+    turtle.left(120)
+    turtle.forward(100)
+elif dict1.get("Shape3")==user_input :
+    turtle.circle(100)
 else:
     print("Enter a value between square, circle and rectangle")
     
